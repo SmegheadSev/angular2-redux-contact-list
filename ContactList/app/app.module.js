725953464bbfa46d676c02a568feb64b63390764
smegheadsev <smegheadsev@DESKTOP-29MO6CE>
2016-10-20 23:19:04 -0500

    Restructure

--- ContactList/app/app.module.js
+++ ContactList/app/app.module.js
@@ -18,11 +18,11 @@ var app_routing_module_1 = require('./app-routing.module');
 var in_memory_web_api_module_1 = require('angular-in-memory-web-api/in-memory-web-api.module');
 var in_memory_data_service_1 = require('./in-memory-data.service');
 var app_component_1 = require('./app.component');
-var dashboard_component_1 = require('./dashboard.component');
-var heroes_component_1 = require('./heroes.component');
-var hero_detail_component_1 = require('./hero-detail.component');
+var dashboard_component_1 = require('./dashboard/dashboard.component');
+var heroes_component_1 = require('./heroes/heroes.component');
+var hero_detail_component_1 = require('./heroes/hero-detail/hero-detail.component');
 var hero_service_1 = require('./hero.service');
-var hero_search_component_1 = require('./hero-search.component');
+var hero_search_component_1 = require('./heroes/hero-search/hero-search.component');
 var AppModule = (function () {
     function AppModule() {
     }
